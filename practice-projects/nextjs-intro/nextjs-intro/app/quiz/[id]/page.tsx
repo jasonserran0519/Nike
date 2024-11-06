d3f7ef03052baede30436c4deea98a777d9935a3
ksamonte50 <ksamonte50@gmail.com>
2024-11-05 22:13:57 -0800

    added styles and routing

---
+++ practice-projects/nextjs-intro/nextjs-intro/app/quiz/[id]/page.tsx
@@ -0,0 +1,9 @@
+function Quiz()
+
+export default function QuizPage({ params }: { params: {id: string} }) {
+    return (
+      <section>
+        <h1>Quiz {params.id}</h1>
+      </section>
+    );
+  }
